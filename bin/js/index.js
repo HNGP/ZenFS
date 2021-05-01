58f4e5a086971fd7f382bebfc3dd20600c5bb47a
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2021-05-01 19:32:08 +0530

    updated branding

--- bin/js/index.js
+++ bin/js/index.js
@@ -1,6 +1,15 @@
 const { app, BrowserWindow } = require('electron')
 function createWindow() {
-    let win = new BrowserWindow({ title: 'Voyager', nodeIntegration: true, icon: "./bin/img/app.png", width: 1200, height: 900, frame: false, transparent: true, vibrancy: 'ultra-dark', webPreferences: { experimentalFeatures: true } })
-    win.loadFile('./bin/html/voyager.html')
+    let win = new BrowserWindow({ 
+        title: 'ZenFS', 
+        nodeIntegration: true, 
+        icon: "./bin/img/app.png", 
+        width: 1200, 
+        height: 900, 
+        frame: false, 
+        transparent: true, 
+        vibrancy: 'ultra-dark', 
+        webPreferences: { experimentalFeatures: true } })
+    win.loadFile('./bin/html/main.html')
 }
 app.on('ready', createWindow)
